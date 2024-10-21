38dfb3e49d57d4874d2f61196317a7c65d0d946a
MN PHANITH <82130727+phanithMN@users.noreply.github.com>
2024-10-21 22:36:56 +0700

    ...

--- resources/views/web-page/my-order/index.blade.php
+++ resources/views/web-page/my-order/index.blade.php
@@ -8,11 +8,11 @@
                 <div class="px-4 bg-fill rounded-top border-bottom">
                   <div class="media align-items-center py-3">
                     <div class="img-thumbnail rounded-circle position-relative" style="width: 6.375rem;">
-                      <img class="rounded-circle" src="https://lh3.googleusercontent.com/a/ACg8ocLckUD5FOwZL05AeKEUqcdXv8R90srDuE9jy6WH3LvJWo_zYJlz=s96-c?sz=150" alt="avatar">
+                      <img class="rounded-circle" src="{{!is_null(Auth::guard('customer')->user()) && isset(Auth::guard('customer')->user()->image) ? asset('uploads/users/' . Auth::guard('customer')->user()->image) : '../assets/img/avatars/1.png'  }}"  alt="avatar">
                     </div>
                     <div class="media-body pl-3">
-                      <h3 class="font-size-base mb-0">MN PHANITH</h3>
-                      <span class="text-accent font-size-sm">phanith.mn@gmail.com</span>
+                      <h3 class="font-size-base mb-0">{{!is_null(Auth::guard('customer')->user()) && isset(Auth::guard('customer')->user()->name) ? Auth::guard('customer')->user()->name : '' }}</h3>
+                      <span class="text-accent font-size-sm">{{!is_null(Auth::guard('customer')->user()) && isset(Auth::guard('customer')->user()->email) ? Auth::guard('customer')->user()->email : '' }}</span>
                     </div>
                   </div>
                 </div>
